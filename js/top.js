f8dac44fc7e753843efdd24de671f13c6e53a320
’mutsumi1009’ <moomoo1009@gmail.com>
2026-01-07 08:59:01 +0900

    1/7作成

---
+++ js/top.js
@@ -0,0 +1,12 @@
+
+  // 生徒さんたちの声
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('.p-top-voice__slider', {
+      type: 'loop',
+      perPage: 1,
+      gap: '2rem',
+      arrows: true,
+      pagination: true,
+    }).mount();
+  });
+  
