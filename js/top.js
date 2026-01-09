2fb27b945acc893ea88118584bd0e36fd0a5b3db
’mutsumi1009’ <moomoo1009@gmail.com>
2026-01-10 08:23:54 +0900

    1/9作成

--- js/top.js
+++ js/top.js
@@ -1,12 +1,19 @@
 
   // 生徒さんたちの声
-document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('DOMContentLoaded', function () {
     new Splide('.p-top-voice__slider', {
       type: 'loop',
-      perPage: 1,
-      gap: '2rem',
+      perPage: 3,      // PCでは3枚
+      gap: '3.5rem',
       arrows: true,
       pagination: true,
+    
+      breakpoints: {
+        767: {         
+          perPage: 1,  
+          gap: '1rem', 
+          
+        },
+      },
     }).mount();
   });
-  
