2775d7e4c74d69462b666517e0d1bc92ec77d58c
’mutsumi1009’ <moomoo1009@gmail.com>
2026-01-15 23:03:18 +0900

    1/15作成

--- js/top.js
+++ js/top.js
@@ -17,3 +17,17 @@
       },
     }).mount();
   });
+
+  // よくあるご質問
+  $(function () {
+    // 最初の回答以外は隠しておく（最初からすべて閉じたい場合は .hide() だけでOK）
+    $(".p-faq__answer").hide();
+  
+    $(".p-faq__question").on("click", function () {
+      // クリックされた質問の次の要素（回答）をスライドで開閉
+      $(this).next(".p-faq__answer").slideToggle(300);
+      
+      // クリックされた質問自体に 'is-open' クラスを付け外しする（矢印の回転用）
+      $(this).toggleClass("is-open");
+    });
+  });
